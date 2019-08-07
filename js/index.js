9d401f2135277048e05fa8ad49ed160a2f9958e8
Aluno <renanandreolla@outlook.com>
2019-08-07 20:29:24 -0300

    Primeiro envio

---
+++ js/index.js
@@ -0,0 +1,4 @@
+//1) Escreva um programa que apresente quatro opções: (a) consulta 
+//saldo, (b) saque e (c) depósito e (d) sair. O saldo deve iniciar
+//em R$ 0,00. A cada saque ou depósito o valor do saldo deve ser
+//atualizado.
